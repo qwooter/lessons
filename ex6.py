fc09d4fc228c39b55ec5e8501df70e9a81b1178d
qwooter <notyrov@gmail.com>
2023-01-24 19:10:24 +0300

    ghj

---
+++ ex6.py
@@ -0,0 +1,7 @@
+n = int(input())
+i = 1
+a = 1
+while i ** 2 <= n:
+    a = i ** 2
+    print(a)
+    i += 1
---
+++ ex7.py
@@ -0,0 +1,7 @@
+x = int(input())
+y = int(input())
+i = 0
+while x < y:
+    x = x 
+    i += 1
+print(i)
